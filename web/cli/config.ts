056da6a93063fbf837c68c4d196ab836ad914122
DR497 <47689875+dr497@users.noreply.github.com>
2021-10-15 15:40:19 +0800

    Remove outdated files

--- web/cli/config.ts
+++
@@ -1,8 +0,0 @@
-import appConfig from '../config'
-
-const RPC_URL = appConfig.rpcUrl
-const PROGRAM_PATH = '../dist/so'
-const STORE_PATH = '../.store'
-const ACCOUNTS_FILE = `${STORE_PATH}/accounts.json`
-
-export { RPC_URL, PROGRAM_PATH, STORE_PATH, ACCOUNTS_FILE }
--- web/config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  programId: 'Gk3pVnNut49YxiKMeDd4gnfimWLDoUnvAS7Zc1pN4jgd',
-  // rpcUrl: 'https://coingrin.com:8080',
-  rpcUrl: 'https://testnet.solana.com',
-  environment: 'test',
-}
